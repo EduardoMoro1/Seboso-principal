102ed3702c95d35137a45068af9a30a7c6ea5c4d
Eduado <129158742+EduardoMoro1@users.noreply.github.com>
2024-09-03 08:34:44 -0300

    finalizado!

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,94 +1,94 @@
 export const perguntas = [
-    {// Pergunta 1 //
+    {// Pergunta 1 
         enunciado: "Numa situação hipotética, você está vivendo em uma pequena fazenda. Qual será sua primeira ação?",
         alternativas: [
             {
-                texto: "Plantar vegetais para vender ou comer posteriormente",
+                texto: "Plantar vegetais para vender ou comer posteriormente.",
                 afirmacao: [
-                    "Você pensa no futuro, quer promover um sustento básico antes de tudo",
+                    "Você pensa no futuro, quer promover um sustento básico antes de tudo.",
                 ],
                 proxima: 1, // Direciona para a pergunta 2
             },
             {
-                texto: "Separar o espaço para o cultivo de animais",
+                texto: "Separar o espaço para o cultivo de animais.",
                 afirmacao: [
-                    "Você pensa como um empreendedor, optou pelo caminho mais lucrativo",
+                    "Você pensa como um empreendedor, optou pelo caminho mais lucrativo.",
                 ],
                 proxima: 2, // Direciona para a pergunta 3
             }
         ]
     },
-    {// Pergunta 2 //
+    {// Pergunta 2 
         enunciado: "Com seu território em mãos, você optaria pelo uso de agrotóxicos e outros produtos químicos?",
         alternativas: [
             {
-                texto: "Sim",
+                texto: "Sim.",
                 afirmacao: [
-                    "Você não se importa com o meio ambiente ou o ambiente onde mora, apenas quer lucro sem esforço",
+                    "Você não se importa com o meio ambiente ou o ambiente onde mora, apenas quer lucro sem esforço.",
                 ],
                 proxima: 3, // Direciona para a pergunta 4
             },
             {
-                texto: "Não",
+                texto: "Não.",
                 afirmacao: [
-                    "Você é um agricultor padrão, opta pela segurança do meio ambiente",
+                    "Você é um agricultor padrão, opta pela segurança do meio ambiente.",
                 ],
                 proxima: 2, // Direciona para a pergunta 3
             }
         ]
     },
-    {// Pergunta 3 //
+    {// Pergunta 3 
         enunciado: "Você optaria por introduzir máquinas na sua fazenda ou fará tudo manualmente?",
         alternativas: [
             {
-                texto: "Irei introduzir o uso de algumas máquinas para processar meus produtos eficientemente",
+                texto: "Irei introduzir o uso de algumas máquinas para processar meus produtos eficientemente.",
                 afirmacao: [
-                    "Você é agora um empreendedor, seu lucro irá provavelmente aumentar muito. Contudo, o meio ambiente sofrerá com suas ações",
+                    "Você é agora um empreendedor, seu lucro irá provavelmente aumentar muito. Contudo, o meio ambiente sofrerá com suas ações.",
                 ],
                 proxima: 4, // Direciona para a pergunta 5
             },
             {
-                texto: "Irei optar pelo uso de minhas mãos somente, para evitar gastos desnecessários",
+                texto: "Irei optar pelo uso de minhas mãos somente, para evitar gastos desnecessários.",
                 afirmacao: [
-                    "Você quer apenas viver por viver, não quer lucro, apenas uma vida simples",
+                    "Você quer apenas viver por viver, não quer lucro, apenas uma vida simples.",
                 ],
                 proxima: 3, // Direciona para a pergunta 4
             }
         ]
     },
-    {// Pergunta 4 //
+    {// Pergunta 4 
         enunciado: "Você pretende ter uma vida simples ou pretende maximizar o lucro?",
         alternativas: [
             {
-                texto: "Vou ter uma vida simples, sem me preocupar com despesas ou maquinário",
+                texto: "Vou ter uma vida simples, sem me preocupar com despesas ou maquinário.",
                 afirmacao: [
-                    "Você é um grande protetor do meio ambiente, está preservando tudo do jeito correto e mantendo o ciclo",
+                    "Você é um grande protetor do meio ambiente, está preservando tudo do jeito correto e mantendo o ciclo.",
                 ],
                 // resposta final
             },
             {
-                texto: "Vou priorizar o lucro acima de tudo, independente das circunstâncias",
+                texto: "Vou priorizar o lucro acima de tudo, independente das circunstâncias.",
                 afirmacao: [
-                    "Você é um grande inimigo do meio ambiente, você não liga para a vegetação, mares, você apenas age sem pensar nos impactos de suas ações",
+                    "Você é um grande inimigo do meio ambiente, você não liga para a vegetação, mares, você apenas age sem pensar nos impactos de suas ações.",
                 ],
                 proxima: 4, // Direciona para a pergunta 5
             }
         ]
     },
-    {// Pergunta 5 //
-        enunciado: "",
+    {// Pergunta 5 
+        enunciado: "Você se vê como uma pessoa que se preocupa com o seu terreno de maneira sustentável?",
         alternativas: [
             {
-                texto: "",
+                texto: "Não, meu terreno possui motivos o suficiente para promover minha alegria e satisfação.",
                 afirmacao: [
-                    "",
+                    "Você é nosso maior inimigo, espero que revise seus valores...",
                 ],
                 // resposta final
             },
             {
-                texto: "",
+                texto: "Sim, apesar de meu grande lucro, eu ainda preciso preservar meu terreno, pois é tudo o que eu tenho.",
                 afirmacao: [
-                    "",
+                    "Você é um grande contribuidor para o nosso processo sustentável de salvar o planeta, obrigado por sua ajuda!",
                 ],
                 // resposta final
             }
